fe1a2515e15ea9c3f161fdbb183f85c9f47a69bd
konbucyber <computer@edu.tuis.ac.jp>
2023-07-12 17:14:54 +0900

    サウンド追加

--- src/components/Sound.tsx
+++ src/components/Sound.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import "./SoundPage.module.css";
 import PlaySound from "use-sound";
 import Sound from "../sounds/miku.wav";
-import CatSE from "../sounds/CatSe.mp3";
-import Kokouno from "../sounds/kokouno.mp3";
+import SE_A from "../sounds/CatSe.mp3";
+import BGM_A from "../sounds/kokouno.mp3";
+import SE_B from "../sounds/decision.mp3";
+import SE_C from "../sounds/item.mp3";
+import BGM_B from "../sounds/labyrinth.mp3";
+import BGM_C from "../sounds/stage.mp3";
 
 const SoundPage: React.FC = () => {
   const [mikuPlay] = PlaySound(Sound, { volume: 1 });
-  const [SE_CatPlay] = PlaySound(CatSE, { volume: 1 });
-  const [KokoPlay] = PlaySound(Kokouno, { volume: 1 });
+  const [SE_APlay] = PlaySound(SE_A, { volume: 1 });
+  const [SE_BPlay] = PlaySound(SE_B, { volume: 1 });
+  const [SE_CPlay] = PlaySound(SE_C, { volume: 1 });
+  const [BGM_APlay] = PlaySound(BGM_A, { volume: 0.8 });
+  const [BGM_BPlay] = PlaySound(BGM_B, { volume: 0.8 });
+  const [BGM_CPlay] = PlaySound(BGM_C, { volume: 0.8 });
 
   return (
     <div>
@@ -24,12 +32,12 @@ const SoundPage: React.FC = () => {
           <tr>
             <td>
               <>
-                <button onClick={() => SE_CatPlay()}>効果音サンプルA</button>
+                <button onClick={() => SE_APlay()}>効果音サンプルA</button>
               </>
             </td>
             <td>
               <>
-                <button onClick={() => KokoPlay()}>BGMサンプルA</button>
+                <button onClick={() => BGM_APlay()}>BGMサンプルA</button>
               </>
             </td>
             <td>
@@ -38,7 +46,30 @@ const SoundPage: React.FC = () => {
               </>
             </td>
           </tr>
-          <tr></tr>
+          <tr>
+            <td>
+              <>
+                <button onClick={() => SE_BPlay()}>効果音サンプルB</button>
+              </>
+            </td>
+            <td>
+              <>
+                <button onClick={() => BGM_BPlay()}>BGMサンプルB</button>
+              </>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <>
+                <button onClick={() => SE_CPlay()}>効果音サンプルC</button>
+              </>
+            </td>
+            <td>
+              <>
+                <button onClick={() => BGM_CPlay()}>BGMサンプルC</button>
+              </>
+            </td>
+          </tr>
         </table>
       </div>
     </div>
